c1029f5883ea0b3c7becb5649e79a32e9a5746ca
Swarnya Srivastava <49378967+Swarnya246@users.noreply.github.com>
2020-07-25 17:12:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
 import { anArrayOfEnglishWords } from './array.js'
+import { anArrayOfSpanishWords } from './array_es.js'
 
 var passphrase = document.getElementById("passphrase")
 var numWords = document.getElementById("numWords")
+var languages = document.getElementById("languages")
 
-function getRandomInt() {
-  return Math.floor(Math.random() * Math.floor(274937));
+function getRandomInt(language) {
+    if (language == "en") {
+        return Math.floor(Math.random() * Math.floor(274937));
+    }
+    else if (language == "es") {
+        return Math.floor(Math.random() * Math.floor(636598));
+    }
+    else if (language == "fr") {
+        return Math.floor(Math.random() * Math.floor(336524));
+    }
 }
 
-const addWords = (words, phrase) => {
+const addWordsEn = (words, phrase) => {
     for (var i = 0; i < words; i++) {
-        var word = anArrayOfEnglishWords[getRandomInt()]
+        var word = anArrayOfEnglishWords[getRandomInt("en")]
         phrase += word + " "
     }
     return phrase
 }
-// make function, input num words needed
 
+const addWordsEs = (words, phrase) => {
+    for (var i = 0; i < words; i++) {
+        var word = anArrayOfSpanishWords[getRandomInt("es")]
+        phrase += word + " "
+    }
+    return phrase
+}
+
+const addWordsEs = (words, phrase) => {
+    for (var i = 0; i < words; i++) {
+        var word = anArrayOfFrenchWords[getRandomInt("fr")]
+        phrase += word + " "
+    }
+    return phrase
+}
+
+const update = () => {
+    if (languages.value == "en") {
+        passphrase.innerText = addWordsEn(numWords.value, "")
+    }
+    else if (languages.value == "fr") {
+        passphrase.innerText = addWordsEn(numWords.value, "")
+    }
+    else if (languages.value == "es") {
+        passphrase.innerText = addWordsEs(numWords.value, "")
+    }
+}
 
 document.addEventListener("load", function(){
-    passphrase.innerText = addWords(5, "")
+    passphrase.innerText = addWordsEn(5, "")
     console.log(passphrase.innerText)
 }); 
 
 numWords.addEventListener("change", function(){
-    passphrase.innerText = addWords(numWords.value, "")
+    update()
+});
+
+languages.addEventListener("change", function(){
+    update()
     console.log(passphrase.innerText)
     console.log(numWords.value)
 });
